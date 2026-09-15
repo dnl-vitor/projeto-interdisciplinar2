d6208cfa06afd9b1874c0373c5ff69c81c20c320
cabra_montanhosa <tsaitama107@gmail.com>
2026-09-14 21:44:36 -0300

    atualizaçao de html e js

--- card.js
+++ card.js
@@ -1,13 +1,16 @@
-document.getElementById ('card.js').addEventListener ('entrar' function(evento) {
+document.getElementById('meu-formulario').addEventListener('submit', function(evento) {
     evento.preventDefault();
-    //é´para evitar de recarregar a pagina
+    // é para evitar de recarregar a pagina
 
-    const email = document.getElementById ('email');
-    const senha = document.getElementById ('senha');
+    const email = document.getElementById('email').value;
+    const senha = document.getElementById('senha').value;
 
     if (email === "teste@email.com" && senha === "123456"){
-        alert("login realizado");
-    }else{
+        
+        // Redireciona o usuário para a página do dashboard
+        location.href = "index_dashbord.html"; 
+        
+    } else {
         alert("senha ou email incorreto");
     }
 });
