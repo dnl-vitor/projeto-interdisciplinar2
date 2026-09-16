8590f697115afc09f6823c7f10db7cbd1e091a9e
cabra_montanhosa <tsaitama107@gmail.com>
2026-09-16 20:34:00 -0300

    atualizaçao de fotos

--- testelogin.js
+++ testelogin.js
@@ -1,6 +1,8 @@
 // Lista com os caminhos das fotos (podem ser locais ou URLs de banco de imagens)
 const imagensPlantas = [
-// --- ESTUFAS E JARDINS BOTÂNICOS ---
+
+/*
+  // --- ESTUFAS E JARDINS BOTÂNICOS ---
     'https://images.unsplash.com/photo-1585320806297-9794b3e4eeae?auto=format&fit=crop&w=1920&q=80',
     'https://images.unsplash.com/photo-1542812596-04a4b4838dec?auto=format&fit=crop&w=1920&q=80',
     'https://images.unsplash.com/photo-1495908333425-29a1e0918c5f?auto=format&fit=crop&w=1920&q=80',
@@ -14,10 +16,19 @@ const imagensPlantas = [
     'https://images.unsplash.com/photo-1463936575829-25148e1db1b8?auto=format&fit=crop&w=1920&q=80',
     'https://images.unsplash.com/photo-1416879595882-3373a0480b5b?auto=format&fit=crop&w=1920&q=80',
     'https://images.unsplash.com/photo-1516481605912-d34c1411504c?auto=format&fit=crop&w=1920&q=80'
-];
-  
-
-function definirFundoAleatorio() {
+*/
+    // --- FOTOS LOCAIS ---
+    'src/imagens/fundo1.jpg',
+    'src/imagens/fundo2.jpg',
+    'src/imagens/fundo3.jpg',
+    'src/imagens/fundo4.jpg',
+    'src/imagens/fundo5.jpg',
+    'src/imagens/fundo6.jpg',
+    'src/imagens/fundo7.jpg'
+  ];
+      
+      
+    function definirFundoAleatorio() {
     // Sorteia um número de 0 até o tamanho do array de imagens
     const indiceAleatorio = Math.floor(Math.random() * imagensPlantas.length);
     const imagemSorteada = imagensPlantas[indiceAleatorio];
